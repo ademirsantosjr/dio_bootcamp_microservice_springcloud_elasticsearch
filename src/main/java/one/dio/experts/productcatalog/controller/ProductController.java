8e6f9290f63c8c2471b6ec712c543b563508935b
Ademir <ademir.m.junior@gmail.com>
2021-09-06 17:16:54 -0300

    Develop GET by id method

--- src/main/java/one/dio/experts/productcatalog/controller/ProductController.java
+++ src/main/java/one/dio/experts/productcatalog/controller/ProductController.java
@@ -1,9 +1,15 @@
 package one.dio.experts.productcatalog.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import one.dio.experts.productcatalog.model.Product;
@@ -17,7 +23,15 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Product create(@RequestBody Product product) {
         return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable int id) {
+        return productRepository.findById(id)
+                                .orElseThrow(
+                                    () -> new NoSuchElementException());
     }
 }
